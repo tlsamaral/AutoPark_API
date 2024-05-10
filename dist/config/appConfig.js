631d72f213c3d2ccf50ed6ddff68bc8e73027512
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2024-05-09 23:03:49 -0300

    Deletando dist

--- dist/config/appConfig.js
+++
@@ -1,4 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true});exports. default = {
-  // url: 'http://localhost:3001',
-  url: process.env.APP_URL,
-};
--- dist/server.js
+++
@@ -1,9 +0,0 @@
-"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _dotenv = require('dotenv'); var _dotenv2 = _interopRequireDefault(_dotenv);
-var _app = require('./app'); var _app2 = _interopRequireDefault(_app);
-_dotenv2.default.config();
-
-const port = process.env.APP_PORT;
-
-_app2.default.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
